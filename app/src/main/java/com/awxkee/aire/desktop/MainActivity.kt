6f77997b0b69ef4172ca6ae489db56effc3b07ac
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-05-17 17:05:09 +0100

    Update pages and api

--- app/src/main/java/com/awxkee/aire/desktop/MainActivity.kt
+++ app/src/main/java/com/awxkee/aire/desktop/MainActivity.kt
@@ -64,31 +64,31 @@ class MainActivity : ComponentActivity() {
                 }
                 LaunchedEffect(key1 = Unit, block = {
                     scope.launch(Executors.newSingleThreadExecutor().asCoroutineDispatcher()) {
-                        val bitmap =
-                            BitmapFactory.decodeResource(resources, R.drawable.haze)
-                                .copy(Bitmap.Config.ARGB_8888, true)
-                                .scaleWith(0.4f)
-
-//                        val bitmap1 =
+//                        val bitmap =
 //                            BitmapFactory.decodeResource(resources, R.drawable.haze)
-//                                .scaleWith(0.6f)
 //                                .copy(Bitmap.Config.ARGB_8888, true)
-////
-//                        scope.launch {
-//                            imagesArray.add(bitmap)
-//                        }
-
-////                        delay(2000L)
+//                                .scaleWith(0.4f)
 //
-//                        var radius = 77
+////                        val bitmap1 =
+////                            BitmapFactory.decodeResource(resources, R.drawable.haze)
+////                                .scaleWith(0.6f)
+////                                .copy(Bitmap.Config.ARGB_8888, true)
+//////
+////                        scope.launch {
+////                            imagesArray.add(bitmap)
+////                        }
+//
+//////                        delay(2000L)
 ////
-                        var time = measureTimeMillis {
-                            val image = Aire.boxBlur(bitmap, 6)
-                            scope.launch {
-                                imagesArray.add(image)
-                            }
-                        }
-                        Log.d("MainActivity", "Exec time $time")
+////                        var radius = 77
+//////
+//                        var time = measureTimeMillis {
+//                            val image = Aire.boxBlur(bitmap, 6)
+//                            scope.launch {
+//                                imagesArray.add(image)
+//                            }
+//                        }
+//                        Log.d("MainActivity", "Exec time $time")
                     }
                 })
 //                var sliderValue by remember { mutableFloatStateOf(100f) }  // Initial value of the slider
